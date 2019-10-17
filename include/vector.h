8b4a63a97e5bb0696ee680f6cf4a62f88cd2403d
akachronix <bgowengaming1@gmail.com>
2019-10-16 21:28:00 -0400

    add bool_vector & unsigned_int_vector

--- include/vector.h
+++ include/vector.h
@@ -2,30 +2,50 @@
 #define VECTOR_H
 
 #include <stddef.h>
+#include <stdbool.h>
 
-typedef struct {
+typedef struct bool_vector {
+	bool* data;
+	size_t capacity;
+	size_t size;
+} bool_vector;
+
+typedef struct char_vector {
 	char* data;
 	size_t capacity;
 	size_t size;
 } char_vector;
 
-typedef struct {
+typedef struct int_vector {
 	int* data;
 	size_t capacity;
 	size_t size;
 } int_vector;
 
-typedef struct {
+typedef struct unsigned_int_vector {
+	unsigned int* data;
+	size_t capacity;
+	size_t size;
+} unsigned_int_vector;
+
+typedef struct float_vector {
 	float* data;
 	size_t capacity;
 	size_t size;
 } float_vector;
 
-typedef struct {
+typedef struct double_vector {
 	double* data;
 	size_t capacity;
 	size_t size;
 } double_vector;
+
+int bool_vector_init(bool_vector* v, size_t init_capacity);
+int bool_vector_destroy(bool_vector* v);
+int bool_vector_realloc(bool_vector* v);
+int bool_vector_push_back(bool_vector* v, bool b);
+int bool_vector_push_back_v(bool_vector* v, int args, ...);
+int bool_vector_shrink_to_fit(bool_vector* v);
 
 int char_vector_init(char_vector* v, size_t init_capacity);
 int char_vector_destroy(char_vector* v);
@@ -40,6 +60,13 @@ int int_vector_realloc(int_vector* v);
 int int_vector_push_back(int_vector* v, int n);
 int int_vector_push_back_v(int_vector* v, int args, ...);
 int int_vector_shrink_to_fit(int_vector* v);
+
+int unsigned_int_vector_init(unsigned_int_vector* v, size_t init_capacity);
+int unsigned_int_vector_destroy(unsigned_int_vector* v);
+int unsigned_int_vector_realloc(unsigned_int_vector* v);
+int unsigned_int_vector_push_back(unsigned_int_vector* v, unsigned int n);
+int unsigned_int_vector_push_back_v(unsigned_int_vector* v, int args, ...);
+int unsigned_int_vector_shrink_to_fit(unsigned_int_vector* v);
 
 int float_vector_init(float_vector* v, size_t init_capacity);
 int float_vector_destroy(float_vector* v);
--- src/vector.c
+++ src/vector.c
@@ -3,6 +3,87 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <stdarg.h>
+#include <stdbool.h>
+
+int bool_vector_init(bool_vector* v, size_t init_capacity)
+{
+	v->data = (bool*)malloc(sizeof(bool) * init_capacity);
+	if (v->data == NULL)
+		return -1;
+	
+	v->capacity = init_capacity;
+	v->size = 0;
+
+	return 0;
+}
+
+int bool_vector_destroy(bool_vector* v)
+{
+	if (v->data == NULL)
+		return -1;
+	
+	free(v->data);
+	v->capacity = 0;
+	v->size = 0;
+
+	return 0;
+}
+
+int bool_vector_realloc(bool_vector* v)
+{
+	v->capacity <<= 2;
+
+	bool* realloc_data = (bool*)realloc(v->data, v->capacity);
+	if (realloc_data == NULL)
+		return -1;
+	
+	v->data = realloc_data;
+	
+	return 0;
+}
+
+int bool_vector_push_back(bool_vector* v, bool b)
+{
+	if (v->size == v->capacity)
+		if (bool_vector_realloc(v) != 0)
+			return -1;
+
+	v->data[v->size] = b;
+	++v->size;
+
+	return 0;
+}
+
+int bool_vector_push_back_v(bool_vector* v, int args, ...)
+{	
+	va_list vl;
+	va_start(vl, args);
+	for (int i = 0; i < args; ++i) {
+		if (v->size == v->capacity)
+			if (bool_vector_realloc(v) != 0)
+				return -1;
+			
+		v->data[v->size] = va_arg(vl, int);
+		++v->size;
+	}
+	
+	va_end(vl);
+	return 0;
+}
+
+int bool_vector_shrink_to_fit(bool_vector* v)
+{
+	if (v->capacity != v->size) {
+		v->capacity = v->size;
+		bool* realloc_data = (bool*)realloc(v->data, v->capacity);
+		if (realloc_data == NULL)
+			return -1;
+		
+		v->data = realloc_data;
+	}
+	
+	return 0;
+}
 
 int char_vector_init(char_vector* v, size_t init_capacity)
 {
@@ -155,6 +236,86 @@ int int_vector_shrink_to_fit(int_vector* v)
 	if (v->capacity != v->size) {
 		v->capacity = v->size;
 		int* realloc_data = (int*)realloc(v->data, v->capacity);
+		if (realloc_data == NULL)
+			return -1;
+		
+		v->data = realloc_data;
+	}
+	
+	return 0;
+}
+
+int unsigned_int_vector_init(unsigned_int_vector* v, size_t init_capacity)
+{
+	v->data = (unsigned int*)malloc(sizeof(unsigned int) * init_capacity);
+	if (v->data == NULL)
+		return -1;
+	
+	v->capacity = init_capacity;
+	v->size = 0;
+
+	return 0;
+}
+
+int unsigned_int_vector_destroy(unsigned_int_vector* v)
+{
+	if (v->data == NULL)
+		return -1;
+	
+	free(v->data);
+	v->capacity = 0;
+	v->size = 0;
+
+	return 0;
+}
+
+int unsigned_int_vector_realloc(unsigned_int_vector* v)
+{
+	v->capacity <<= 2;
+
+	unsigned int* realloc_data = (unsigned int*)realloc(v->data, v->capacity);
+	if (realloc_data == NULL)
+		return -1;
+	
+	v->data = realloc_data;
+	
+	return 0;
+}
+
+int unsigned_int_vector_push_back(unsigned_int_vector* v, unsigned int n)
+{
+	if (v->size == v->capacity)
+		if (unsigned_int_vector_realloc(v) != 0)
+			return -1;
+
+	v->data[v->size] = n;
+	++v->size;
+
+	return 0;
+}
+
+int unsigned_int_vector_push_back_v(unsigned_int_vector* v, int args, ...)
+{
+	va_list vl;
+	va_start(vl, args);
+	for (int i = 0; i < args; ++i) {
+		if (v->size == v->capacity)
+			if (unsigned_int_vector_realloc(v) != 0)
+				return -1;
+			
+		v->data[v->size] = va_arg(vl, unsigned int);
+		++v->size;
+	}
+	
+	va_end(vl);
+	return 0;
+}
+
+int unsigned_int_vector_shrink_to_fit(unsigned_int_vector* v)
+{
+	if (v->capacity != v->size) {
+		v->capacity = v->size;
+		unsigned int* realloc_data = (unsigned int*)realloc(v->data, v->capacity);
 		if (realloc_data == NULL)
 			return -1;
 		
